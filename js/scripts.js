aee29de2fd71e7dc85052e1f766780d91d746ca4
Ivan B <va-n-ja@yandex.ru>
2017-06-06 18:10:21 +0500

    Скрипт открытия/закрытия навигации

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var menuBtn = document.querySelector(".main-nav__toggler span");
+var menuBtnWrap = document.querySelector(".main-nav__toggler");
+var menu = document.querySelector(".main-nav__list");
+
+menuBtn.addEventListener("click", function() {
+  menuBtnWrap.classList.toggle("main-nav__toggler--active");
+  menu.classList.toggle("main-nav__list--visible");
+});
