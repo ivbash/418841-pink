00e9b28ef20c96e87d35cbded9dd1849af52f4d0
Ivan B <va-n-ja@yandex.ru>
2017-06-14 10:37:39 +0500

    Меню открыто при отключенном js

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ var menuBtn = document.querySelector(".main-nav__toggler span");
 var menuBtnWrap = document.querySelector(".main-nav__toggler");
 var menu = document.querySelector(".main-nav__list");
 
+menuBtnWrap.classList.toggle("main-nav__toggler--active");
+menu.classList.toggle("main-nav__list--visible");
+
 menuBtn.addEventListener("click", function() {
   menuBtnWrap.classList.toggle("main-nav__toggler--active");
   menu.classList.toggle("main-nav__list--visible");
